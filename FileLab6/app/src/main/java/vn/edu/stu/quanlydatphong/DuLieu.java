646ee731ee917cbb53c718fb38445078bf6af4a7
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-11-25 14:35:34 +0700

    cập nhật DuLieu.java

--- FileLab6/app/src/main/java/vn/edu/stu/quanlydatphong/DuLieu.java
+++ FileLab6/app/src/main/java/vn/edu/stu/quanlydatphong/DuLieu.java
@@ -1,4 +1,30 @@
 package vn.edu.stu.quanlydatphong;
 
+import java.util.ArrayList;
+
+import vn.edu.stu.quanlydatphong.model.DatPhong;
+
 public class DuLieu {
+    public static ArrayList<DatPhong> dsDatPhong;
+
+    static {
+        dsDatPhong = new ArrayList<>();
+    }
+
+    public static DatPhong layDatPhong(int index) {
+        if (index >= 0 && index < dsDatPhong.size()) {
+            return dsDatPhong.get(index);
+        }
+        return null;
+    }
+
+    public static void themDatPhong(DatPhong dp) {
+        dsDatPhong.add(dp);
+    }
+
+    public static void xoaDatPhong(int index) {
+        if (index >= 0 && index < dsDatPhong.size()) {
+            dsDatPhong.remove(index);
+        }
+    }
 }
